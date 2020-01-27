39022a267fdf1e0b16ffad863ca0dc3bda89ec8f
iMakca <urk.maksim1@yandex.ru>
2020-01-27 19:40:58 +0200

    add progect

---
+++ SearchPhoto/MainTabbarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabbarController.swift
+//  SearchPhoto
+//
+//  Created by Maxim Granchenko on 27.01.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+    }
+    
+}
